22ea0f1551a8946ca6882c34382a63042138620e
brmsAdmin <>
2018-01-08 12:11:38 +0530

    {/policyquotenew/src/main/java/org/acme/insurancenew/Rejection.java}

--- policyquotenew/src/main/java/org/acme/insurancenew/Rejection.java
+++ policyquotenew/src/main/java/org/acme/insurancenew/Rejection.java
@@ -10,8 +10,26 @@ public class Rejection implements java.io.Serializable
 
    static final long serialVersionUID = 1L;
 
+   @org.kie.api.definition.type.Label(value = "Reason")
+   private java.lang.String reason;
+
    public Rejection()
    {
+   }
+
+   public java.lang.String getReason()
+   {
+      return this.reason;
+   }
+
+   public void setReason(java.lang.String reason)
+   {
+      this.reason = reason;
+   }
+
+   public Rejection(java.lang.String reason)
+   {
+      this.reason = reason;
    }
 
 }
